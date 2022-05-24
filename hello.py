6818cab16c75a2730a5b33bc1b07438a15620773
Eduard <eduardolteanu@gmail.com>
2022-05-24 20:07:25 +0300

    Home page and about page

--- hello.py
+++ hello.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h2>Home page for the moment!</h2>"
+
+@app.route("/about")
+def about():
+    return "<h2>About page for the moment!</h2>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
