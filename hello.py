2da65275ea116ada6ac7a98b42e07b61204c5169
Eduard <eduardolteanu@gmail.com>
2022-05-24 22:32:15 +0300

    Implementare Boostrap plus Layout
    Am creat layout pentru a putea crea o relatie parinte copil cu restul paginilor care urmeaza sa fie adaugate

--- hello.py
+++ hello.py
@@ -3,13 +3,20 @@ app = Flask(__name__)
 
 aplicatie = [
     {
-        ''
+        'Application_ID': '1',
+        'Application_Name': 'Google Drive',
+        'Install_command': 'choco install google-drive-file-stream',
+    },
+   {
+        'Application_ID': '2',
+        'Application_Name': ' Discord',
+        'Install_command': 'choco install discord',
     }
 ]
 
 @app.route("/")
 def hello_world():
-    return render_template("hello.html")
+    return render_template('home.html', aplicatie=aplicatie)
 
 @app.route("/about")
 def about():
