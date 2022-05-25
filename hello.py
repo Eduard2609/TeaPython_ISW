7ef87ea8427f6fac70aca3e2cf3550e6c9edfa8d
Eduard <eduardolteanu@gmail.com>
2022-05-25 17:49:07 +0300

    incerc sa rezolv bug ul cu page ul admin

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, request, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from login import RegistrationForm, LoginForm
-from datetime import datetime
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
