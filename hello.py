c15af52c0bbb4261b27e4736ba23d9b6ae5946ea
Alex Momoi <alexandru.momoi@student.unitbv.ro>
2022-05-25 14:00:50 +0300

    push

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, request, flash, redirect
 from login import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' 
 
 aplicatie = [
     {
