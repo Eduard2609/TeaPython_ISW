830fdef6f6b00173f2411c0528eda3893307a23e
Alex Momoi <alexandru.momoi@student.unitbv.ro>
2022-05-25 03:18:48 +0300

    added login and register functions

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect
+from login import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 aplicatie = [
     {
@@ -48,6 +50,27 @@ def home():
 @app.route("/about")
 def about():
     return  render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'alexandru.momoi@student.unitbv.ro' and form.password.data == 'parola':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
