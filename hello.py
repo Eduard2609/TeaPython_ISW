09c7f7a80194ff3f045129b86f2dc55e8beb2427
Eduard <eduardolteanu@gmail.com>
2022-05-25 15:06:29 +0300

    mai usor de citit si explicat cu niste comentarii

--- hello.py
+++ hello.py
@@ -1,8 +1,66 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from login import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' 
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///TeaBase.db'
+db = SQLAlchemy(app)
+
+# ---------------- Database model ORM ---------------- #
+
+class User(db.Model):
+    Users_id = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(30), unique=True, nullable=False)
+    Password = db.Column(db.String(225), nullable=False)
+    Email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.Username}', '{self.Email}')"
+
+class Applications(db.Model):
+    Applications_id = db.Column(db.Integer, primary_key=True)
+    Application_name = db.Column(db.String(120), unique=True, nullable=False)
+    Install_command = db.Column(db.String(225), nullable=False)
+    Image_file = db.Column(db.String(225), nullable=False, default='default.png')
+
+    def __repr__(self):
+        return f"Application('{self.Application_name}', '{self.Install_command}', '{self.Image_file}')"
+
+class Genres(db.Model):
+    Genres_id = db.Column(db.Integer, primary_key=True)
+    Genres_name = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Genre('{self.Genre_name}')"
+
+class AppsGenres(db.Model):
+    AppsGenres_id = db.Column(db.Integer, primary_key=True)
+    Application_id = db.Column(db.Integer, db.ForeignKey('Applications.Applications_id'), nullable=False)
+    Genre_id = db.Column(db.Integer, db.ForeignKey('Genres.Genres_id'), nullable=False)
+
+    def __repr__(self):
+        return f"AppsGenres('{self.Application_id}', '{self.Genre_id}')"
+
+class Presets(db.Model):
+    Presets_id = db.Column(db.Integer, primary_key=True)
+    Preset_name = db.Column(db.String(120), unique=True, nullable=False)
+    Users_id = db.Column(db.Integer, db.ForeignKey('User.Users_id'), nullable=False)
+
+    def __repr__(self):
+        return f"Preset('{self.Preset_name}', '{self.Users_id}')"
+
+class AppsPresets(db.Model):
+    AppsPresets_id = db.Column(db.Integer, primary_key=True)
+    Presets_id = db.Column(db.Integer, db.ForeignKey('Presets.Presets_id'), nullable=False)
+    Application_id = db.Column(db.Integer, db.ForeignKey('Applications.Applications_id'), nullable=False)
+
+    def __repr__(self):
+        return f"AppsPresets('{self.Presets_id}', '{self.Application_id}')"
+
+# ---------------- Database model ORM -----END ---------------- #
+
+# ---------------- Routes and others -------------------------- #
 
 aplicatie = [
     {
