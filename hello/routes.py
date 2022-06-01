17a4f3103647075dd290a14ba4a8eddaabd49b29
Alex Momoi <alexandru.momoi@student.unitbv.ro>
2022-06-02 00:20:12 +0300

    order apps by name

--- hello/routes.py
+++ hello/routes.py
@@ -33,7 +33,7 @@ def home():
     # image_file = url_for('static', filename='app_pics/default.jpg')
     # aplicatie = Application.query.all()
     page = request.args.get('page', 1, type=int)
-    aplicatie = Application.query.paginate(per_page=2)
+    aplicatie = Application.query.order_by(Application.name).paginate(page=page, per_page=5) # order by app name
     return render_template('home.html', aplicatie=aplicatie)
 
 
