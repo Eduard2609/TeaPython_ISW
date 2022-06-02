7ce4e15f6b120bb415b7acfa5fec240e0cefefd8
Eduard <eduardolteanu@gmail.com>
2022-06-02 20:55:24 +0300

    Cart system added

--- hello/main/routes.py
+++ hello/main/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, request
+from flask import render_template, request, session, flash, redirect, url_for
 from hello.models import Application
 from flask import Blueprint
 
@@ -16,3 +16,36 @@ def home():
 @main.route("/about")
 def about():
     return render_template('about.html')
+
+@main.route("/home/<int:id_app>")
+def app(id_app):
+    app = Application.query.get_or_404(id_app)
+    return render_template('home.html', app=app)
+
+@main.route("/cart")
+def shoping_cart():
+    if "cart" not in session:
+        flash ("Your cart is empty", "info")
+        return render_template('cart.html', display_cart={})
+    else:
+        apps = session["cart"]
+        dict_of_apps = {}
+
+        for app in apps:
+            application = Application.query.get(app)
+    return render_template('cart.html', display_cart=dict_of_apps)
+
+@main.route("/add_to_cart/<int:id_app>")
+def add_to_cart(id_app):
+    if "cart" not in session:
+        session["cart"] = []
+    session["cart"].append(id_app)
+    flash ("Added to cart", "success")
+    return redirect(url_for("main.home"))
+
+@main.route("/download")
+def download():
+    for app in session["cart"]:
+        application = Application.query.get(app)
+        print(application.install_command)
+    return render_template('download.html')
