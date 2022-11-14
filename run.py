0c2d457448b74f1440b582deaa8efcbbb1842438
Eduard <eduardolteanu@gmail.com>
2022-11-14 16:12:44 +0200

    req

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
 from hello import create_app
-import asyncio
 
 app = create_app()
 
-async def main():
-    if __name__ == '__main__':
-        app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-asyncio.run(main())
 
