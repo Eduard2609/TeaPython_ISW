a1533e14c34d54d6a16051d7b365504815646234
Eduard <eduardolteanu@gmail.com>
2022-11-23 16:34:00 +0200

    changed were file is genereated

--- project/installscript/install.py
+++ project/installscript/install.py
@@ -1,13 +1,17 @@
 #generate a bin file with a given item from a list that includes the item
 def generate_bin_file(item):
     #open the file to write
-    f = open('install.bin', 'w')
+    f = open('bin/install.bin', 'w')
     #convert list to string
     item = ''.join(item)
     #write the item to the file
     f.write(item)
+    #change save file location
     #close the file
     f.close()
+    
 
+
+    
 #call generate_bin_file with the item 'test'
-#generate_bin_file('test')
+generate_bin_file('test')
