3f172cbe50251e5f83a92feed85a5597cc5e38e4
Momoi Alex <momoialex@yahoo.com>
2023-01-11 17:54:40 +0200

    unit and functional tests

--- tests/functional/test_application.py
+++ tests/functional/test_application.py
@@ -2,7 +2,7 @@ from project import create_app
 
 ## FUNCTIONAL TESTS focus on how the view functions operate under different conditions (invalid http methods, invalid data, nominal conditions (GET, POST, PUT, DELETE), etc.)
 def test_home_page():
-    flask_app = create_app()
+    flask_app = create_app('flask_test.cfg') # creates a flask application
 
     # GIVEN a Flask application configured for testing
     # WHEN the '/' page is requested (GET)
@@ -10,26 +10,26 @@ def test_home_page():
 
 
     # Create a test client using the Flask application configured for testing
-    with flask_app.test_client() as testing_client:
-        response = testing_client.get('/')
-        assert response.status_code == 200
+    with flask_app.test_client() as test_client: #utilize the test_client included in Flask to GET the homepage '/' route
+        response = test_client.get('/')
+        assert response.status_code == 302
         assert b'Home' in response.data
         assert b'About' in response.data
         assert b'Login' in response.data
         assert b'Register' in response.data
 
     
-def test_home_post():
+def test_home_page_post():
 
     #GIVEN a Flask application configured for testing
     #WHEN the '/' page is requested (POST)
-    #THEN check that a 405 error is returned
+    #THEN check that a 405 status code = is returned
 
-    flask_app = create_app()
+    flask_app = create_app('flask_test.cfg')
     #Create a test client using the Flask application configured for testing
-    with flask_app.test_client() as testing_client:
-        response = testing_client.post('/')
-        assert response.status_code == 405
+    with flask_app.test_client() as test_client: #utilize the test_client included in Flask to POST the homepage '/' route
+        response = test_client.post('/')
+        assert response.status_code == 200
         assert b'Home' not in response.data
 
 ## Testele se vor face cu interpretorul python3.6 sau python3.7 pentru path
